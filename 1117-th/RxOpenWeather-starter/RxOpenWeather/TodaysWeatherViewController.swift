44973f3188eb50e9ec9d35e48efc673032cd7c30
Jay <jaypee.1797@gmail.com>
2016-11-17 13:05:50 -0800

    Http request using FRP

--- 1117-th/RxOpenWeather-starter/RxOpenWeather/TodaysWeatherViewController.swift
+++ 1117-th/RxOpenWeather-starter/RxOpenWeather/TodaysWeatherViewController.swift
@@ -11,19 +11,28 @@ import RxSwift
 import RxCocoa
 
 class TodaysWeatherViewController: UIViewController {
-  
-  @IBOutlet weak var cityTextField: UITextField!
-  @IBOutlet weak var forecastButton: UIButton!
-  
-  @IBOutlet weak var descriptionLabel: UILabel!
-  @IBOutlet weak var minTemperatureLabel: UILabel!
-  @IBOutlet weak var maxTemperatureLabel: UILabel!
-  @IBOutlet weak var avgTemperatureLabel: UILabel!
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-
-  
+    
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var forecastButton: UIButton!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var minTemperatureLabel: UILabel!
+    @IBOutlet weak var maxTemperatureLabel: UILabel!
+    @IBOutlet weak var avgTemperatureLabel: UILabel!
+    
+    let disposeBag = DisposeBag()
+    let openWeatherMapAPI = RxOpenWeatherMapAPI()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupObservers()
+    }
+    
+    func setupObservers() {
+        let maybeWeatherObservable: Observable<Weather?> = cityTextField.rx.text.asObservable().throttle(0.75, scheduler: MainScheduler.instance).flatMapLatest { (weather: Weather)
+            
+        }
+    }
+    
 }
 
